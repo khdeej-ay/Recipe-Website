9b35d1be06cc95f8d992f4db2071b466c9e818e1
Khadeeja <s.khdeej@gmail.com>
2025-01-17 21:12:11 +0400

    Create media/index.js

---
+++ Frontend/src/media/index.js
@@ -0,0 +1,14 @@
+// fruit bowl
+import Carousel_1 from "./nihari.jpg";
+// pancakes
+import Carousel_2 from "./mushrooms.jpg"; 
+// pasta
+import Carousel_3 from "./noodles.jpg";
+// salad?
+import Carousel_4 from "./pancakes.jpg";
+// hamburger
+import Carousel_5 from "./dumplings.jpg";
+// sushi
+import Carousel_6 from "./veggies.jpg";
+
+export { Carousel_1, Carousel_2, Carousel_3, Carousel_4, Carousel_5, Carousel_6 };
