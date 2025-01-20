5a956028d44b7d068ba9d4f4495fc142df8b0f52
Khadeeja <s.khdeej@gmail.com>
2025-01-20 14:29:36 +0400

    Update api/index.js

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -17,3 +17,34 @@ export const fetchCategories = async () => {
     const data = await response.json();
     return data;
 };
+
+// export const fetchRandomRecipe = async (ingredient) => {
+//     const response = await fetch(
+//         `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+//     );
+//     if (!response.ok) {
+//         throw new Error('Failed to fetch recipes');
+//     }
+//     const data = await response.json();
+
+//     // If no meals are returned
+//     if (!data.meals || data.meals.length === 0) return null;
+
+//     // Fetch detailed recipe for one random meal
+//     const randomMeal = data.meals[Math.floor(Math.random() * data.meals.length)];
+//     const detailResponse = await fetch(
+//         `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${randomMeal.idMeal}`
+//     );
+//     const detailData = await detailResponse.json();
+//     return detailData;
+// };
+
+export const fetchRandomRecipe = async (ingredient) => {
+    const response = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+    );
+    if (!response.ok) {
+        throw new Error('Failed to fetch');
+    }
+    return await response.json();
+};
