9c1197d369533b222fc638c8214e7afaeac7c684
Khadeeja <s.khdeej@gmail.com>
2025-01-20 15:01:00 +0400

    Update RecipeInfo.jsx

--- Frontend/src/components/RecipeInfo.jsx
+++ Frontend/src/components/RecipeInfo.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
 
 const RecipePage = () => {
   const { idMeal } = useParams();  // Get the recipe id from URL params
