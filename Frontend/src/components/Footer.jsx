cdd3e2174551b904d4decbacbd6ae0a06c0ae90f
Khadeeja <s.khdeej@gmail.com>
2025-01-20 14:27:24 +0400

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className="text-white bg-neutral-800">
+        <footer className="text-neutral-100 bg-black">
             
-            <div className="flex items-center justify-center py-7">
+            <div className="flex items-center justify-center py-5">
                 &copy; Syeda Khadeeja, 2025
             </div>
         </footer>
