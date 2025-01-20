b91381cfeb8e838d42e5b443b8da6d22ab78fd50
Khadeeja <s.khdeej@gmail.com>
2025-01-20 14:28:58 +0400

    Update RecipePage.jsx

--- Frontend/src/pages/RecipePage.jsx
+++ Frontend/src/pages/RecipePage.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import RecipeInfo from '../components/RecipeInfo'
+import Footer from '../components/Footer'
 
 const RecipePage = () => {
   return (
     <div>
-      
+      <main className='w-full flex flex-col'>
+
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Recipe Info section */}
+        <section id="recipe-info" className='w-full mx-auto'>
+          <RecipeInfo />
+        </section>
+
+        {/* Footer */}
+        <Footer />
+
+      </main>
     </div>
   )
 }
